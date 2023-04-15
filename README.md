70c277f54407fc7afc95d13c3453ee1b7e55c802
Deepen Dhulla <deepen@deependhulla.com>
2023-04-15 14:09:09 +0530

    update on Saturday 15 April 2023 02:09:09 PM IST

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # n2n-debian11
+
+Ntop -- n2n peer- to peer installation on debian 11 with latest versiob
+
---
+++ n2n-setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+apt-get install build-essential autogen autoconf vim screen iptraf-ng etthool net-tools
+
+cd /opt/ ;
+git clone https://github.com/ntop/n2n
+cd /opt/n2n/
+./autogen.sh
+./configure
+make
+
+## see and check command
+#/opt/n2n/supernode
+#/opt/n2n/edge
+
+## for node 1 and node 2 use for support
+#/opt/n2n/edge -c mydnnetwork -k myspecialkeyhere -a 192.168.55.20  -l sdn.internetdomainname.com:123456
+#/opt/n2n/edge -c mysdnnetwork -k mysepcialkeyhere -a 192.168.55.10  -l sdn.internetdomainname.com:123456
+
+
+
